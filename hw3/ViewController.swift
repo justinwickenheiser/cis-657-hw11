616512224bef1a4ff75f976bc9d99c20eec43efc
Justin Wickenheiser <wickenju@gvsu.edu>
2018-05-15 18:18:28 -0400

    Initial Commit

---
+++ hw3/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hw3
+//
+//  Created by Justin Wickenheiser on 5/15/18.
+//  Copyright © 2018 Justin Wickenheiser. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
