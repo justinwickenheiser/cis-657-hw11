117f25784da790b9ea50f126f789b62a4b7da014
Justin Wickenheiser <wickenju@gvsu.edu>
2018-05-15 18:45:04 -0400

    Set up base layout and outlet/action declarations

--- hw3/ViewController.swift
+++ hw3/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var latP1: DecimalMinusTextField!
+    @IBOutlet weak var longP1: DecimalMinusTextField!
+    @IBOutlet weak var latP2: DecimalMinusTextField!
+    @IBOutlet weak var longP2: DecimalMinusTextField!
+    @IBOutlet weak var distance: UILabel!
+    @IBOutlet weak var bearing: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // dismiss keyboard when tapping outside of text fields
+        let detectTouch = UITapGestureRecognizer(target: self, action:
+            #selector(self.dismissKeyboard))
+        self.view.addGestureRecognizer(detectTouch)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    // Simple function to dismiss keyboard
+    @objc func dismissKeyboard() {
+        self.view.endEditing(true)
+    }
 
+    @IBAction func calculate(_ sender: Any) {
+    }
+    
+    @IBAction func clear(_ sender: Any) {
+    }
+    
 
 }
 
